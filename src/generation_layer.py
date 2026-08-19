72ae6974a287ff21b57ccb7b0686cbea83c60847
abdoomootaa <abdelrahmanabdeen50@gmail.com>
2026-08-19 14:13:46 +0300

    add API Key

--- src/generation_layer.py
+++ src/generation_layer.py
@@ -491,7 +491,7 @@ class RAGPipeline:
         chroma_path: str = "data/chroma_db",
         collection_name: str = "clinical-guidelines",
         embeddings_model: str = "pritamdeka/S-PubMedBert-MS-MARCO",
-        groq_api_key: str = None,
+        groq_api_key: str = "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",
         llm_model: str = "llama-3.3-70b-versatile",
         groq_base_url: str = "https://api.groq.com/openai/v1",
     ):
